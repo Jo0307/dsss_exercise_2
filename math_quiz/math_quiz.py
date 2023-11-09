0b38081f6c882468775971d6ed91087b1657dde8
Jo0307 <seongyeun.jo@gmail.com>
2023-11-09 16:05:15 +0100

    <math_quiz_korrekt

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,111 @@
 import random
 
 
-def function_A(min, max):
+def random_num(min_num, max_num):
     """
-    Random integer.
-    """
-    return random.randint(min, max)
+
+    :return: The random number in range (min, max)
+    :rtype: int
 
 
-def function_B():
+    """
+    return random.randint(min_num, max_num)
+
+
+def random_operator():
+    """"
+    Choose one arithmetic operator for calculate
+
+    :return: The random arithmetic operator
+    :rtype: float
+
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def func_num_operator(num_1, num_2, calcul_oper):
+    """
+    The name of create function with chosen random numbers and arithmetic operators
+    Calculate the result of chosen random numbers and operator
+    :Args:
+    :param func_name: The name of create function
+    :type func_name: string
+
+    :retrun: The name of create function and The calculated result of function
+    :rtype: string, int
+
+    """
+    func_name = f"{num_1} {calcul_oper} {num_2}"
+    if calcul_oper == '+':
+        func_res = num_1 + num_2
+    elif calcul_oper == '-':
+        func_res = num_1 - num_2
+    else:
+        func_res = num_1 * num_2
+    return func_name, func_res
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Calculate the score of math quiz und print it
+
+    :param score: The score of the math quiz
+    :type score : int
+
+    :param total_quiz : The number of quizzes run
+    :type total_quiz : int
+
+    for _ in range(total_quiz) : run the quiz total_quiz times
+
+    :param num_1: random number in range (1,10)
+    :type num_1: int
+
+    :param num_2: random number in range (1,5)
+    :type num_2: int
+
+    :param operator: choose one operator for calculate
+    :type operator: float
+
+    :param problem: The name of random create function
+    :type problem: string
+
+    :param correct_ans: The correct answer of problem
+    :type correct_ans: int
+
+    :unser_Ans: in terminal input
+
+    :if unser_Ans is correct,
+     score += 1 : score will be counted +1 according to correct mathquiz
+     if not,
+     go exception.
+
+    """
+
+
+    score = 0
+    total_quiz = 5
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_quiz):
+        num_1 = random_num(1, 10)
+        num_2 = random_num(1, 5)
+        operator = random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, correct_ans = func_num_operator(num_1, num_2, operator)
+        print(f"\nQuestion: {problem}")
+        user_Ans = input("Your answer: ")
+        user_Ans = int(user_Ans)
 
-        if useranswer == ANSWER:
+        if user_Ans == correct_ans:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {correct_ans}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_quiz}")
+
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_num, random_operator, func_num_operator
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = random_num(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
@@ -19,7 +19,10 @@ class TestMathGame(unittest.TestCase):
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
+                (6, 7, '-', '6 + 7', 13),
+
                 ''' TODO add more test cases here '''
+
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
